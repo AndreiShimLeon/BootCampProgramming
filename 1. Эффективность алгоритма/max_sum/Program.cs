9c0a61c8a05fe0f7d69d8dd35884cfc95154e042
AndreiShimLeon <art.shim@yandex.ru>
2023-04-01 14:22:14 +0300

    initial commit

---
+++ 1. Эффективность алгоритма/max_sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using static ArrayCreator;
+using static ArrayMath;
+using System.Diagnostics;
+
+int[] array = 1_000_000.Create() // создает массив из 10 элементов
+                .Fill(1,10);
+// array.ConvertToStringAndPrintToTerminal();
+// Console.WriteLine(array.ConvertToStringAndPrintToTerminal());
+
+int m = 100_000;
+Stopwatch sw = new Stopwatch();
+sw.Start();
+int max  = array.BadGetSum(m);
+sw.Stop();
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+sw.Reset();
+sw.Start();
+max  = array.GoodGetSum(m);
+sw.Stop();
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
